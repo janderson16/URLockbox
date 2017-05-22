0f3b5a2f16d26d4f0bdd9bb0b439ffc75701df89
James Anderson <james.p.anderson16@gmail.com>
2017-05-22 12:54:09 -0600

    adds create/edit links functionality, only working on backend at present

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,45 @@ class LinksController < ApplicationController
   def index
     unless current_user
       redirect_to "/login"
+    else
+      @user = current_user
+      @links = @user.links.all
     end
+  end
+
+  def create
+    user = current_user
+    link = user.links.new(link_params)
+
+    if link.save
+      render partial: 'links/show-link', locals: {link: link}, layout: false
+    else
+      flash[:notice] = "Invalid URL!"
+      render :new, status: 500
+    end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      flash[:success] = "Link successfully updated"
+      redirect_to links_path
+    elsif @link.title == ""
+      flash[:notice] = "Title cannot be blank"
+      render :edit
+    else
+      flash[:notice] = "Please check that URL is a valid address"
+      render :edit
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :title, :user_id)
   end
 end
